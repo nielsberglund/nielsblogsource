441411a11bcab7508b0948a2ebb14f4e6870febb
Niels Berglund <niels.it.berglund@gmail.com>
2019-08-03 09:26:39 +0200

    New blog-post - S2k19 CTP 3.2  - Java

--- _scratchpad/test_java_install.sql
+++ _scratchpad/test_java_install.sql
@@ -17,18 +17,24 @@ CREATE EXTERNAL LANGUAGE Java
 FROM (CONTENT = 'W:\java-lang-extension-3.2.zip', FILE_NAME = 'javaextension.dll');
 GO
 
+SELECT * FROM sys.external_languages
+
 CREATE EXTERNAL LIBRARY javaSDK 
 FROM (CONTENT = 'W:\mssql-java-lang-extension-3.2.jar')
 WITH (LANGUAGE = 'Java');
 GO 
 
+SELECT * FROM sys.external_libraries
+
 CREATE EXTERNAL LIBRARY javaTest
 FROM (CONTENT = 'W:\sql-1.0.jar')
 WITH (LANGUAGE = 'Java');
 
+SELECT * FROM sys.external_libraries
+
 -- test that it works
 DECLARE @p1 int = 21;
-DECLARE @p2 int = 11;
+DECLARE @p2 int = 21;
 EXEC sp_execute_external_script
   @language = N'Java'
 , @script = N'sql.JavaTest1'
